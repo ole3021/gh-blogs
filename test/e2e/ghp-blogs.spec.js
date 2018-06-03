f5f8c535b363d3fc8e22a679c8836e4f46dc633a
Oliver.W <ole3021@gmail.com>
2018-06-03 11:43:00 +0800

    init lib method

--- test/e2e/ghp-blogs.spec.js
+++ test/e2e/ghp-blogs.spec.js
@@ -1,16 +1,26 @@
+const path = require('path');
+const fs = require('fs');
 const GHBlogs = require('../../');
 
 describe('GHBlogs', () => {
-  it('should can create instance with correct props', () => {
-    const myBlogs = new GHBlogs(
-      'ole3021.me',
-      './fixture/blogs',
-      './fixture/blogIndex.db'
-    );
+  const folderPath = path.join(__dirname, '../fixture/blogs');
+  const dbPath = path.join(__dirname, '../fixture/db/blog.db');
+  const testPath = path.join(__dirname, '../fixture/db/test.db');
 
+  const myBlogs = new GHBlogs('ole3021.me', folderPath, dbPath);
+
+  beforeAll(() => {
+    if (fs.existsSync(dbPath)) fs.unlinkSync(dbPath);
+  });
+
+  afterAll(() => {
+    if (fs.existsSync(dbPath)) fs.unlinkSync(dbPath);
+  });
+
+  it('should can create instance with correct props', () => {
     expect(myBlogs.host).toEqual('ole3021.me');
-    expect(myBlogs.folderPath).toEqual('./fixture/blogs');
-    expect(myBlogs.indexFile).toEqual('./fixture/blogIndex.db');
+    expect(myBlogs.folderPath).toEqual(folderPath);
+    expect(myBlogs.indexFile).toEqual(dbPath);
   });
 
   it('should throw error when missing required field', () => {
@@ -19,5 +29,27 @@ describe('GHBlogs', () => {
     } catch (error) {
       expect(error.message).toEqual('The host or the blogPath are quired.');
     }
+  });
+
+  it('should generate index for lbogs', async () => {
+    const beforeState = fs.existsSync(dbPath);
+
+    await myBlogs.generateIndex();
+    const laterState = fs.existsSync(dbPath);
+    expect(beforeState).toBe(false);
+    expect(laterState).toBe(true);
+  });
+
+  it('should generate index for lbogs', async () => {
+    const content = JSON.parse(fs.readFileSync(testPath));
+    const myBlog = new GHBlogs('ole3021.me', folderPath);
+
+    myBlog.loadJSON(content);
+    const blogs = myBlog.fetchAll();
+
+    expect(blogs.length).toEqual(3);
+    expect(blogs[0]._id).toEqual('about');
+    expect(blogs[1]._id).toEqual('micro-service-with-node-js');
+    expect(blogs[2]._id).toEqual('Zldfjz');
   });
 });
